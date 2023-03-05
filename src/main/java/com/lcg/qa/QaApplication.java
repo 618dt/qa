d2af2280b28861c5160f1228663f328a6785688b
618dt <860131794@qq.com>
2023-03-05 20:57:26 +0800

    完成用户注册登录部分

--- src/main/java/com/lcg/qa/QaApplication.java
+++ src/main/java/com/lcg/qa/QaApplication.java
@@ -1,9 +1,13 @@
 package com.lcg.qa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.lcg"})
+@MapperScan("com.lcg.qa.mapper")
 public class QaApplication {
 	//启动类
 	public static void main(String[] args) {
